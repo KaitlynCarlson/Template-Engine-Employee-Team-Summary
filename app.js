aa6af66dad9e01bcc367cf321dd4d8f765098b82
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-02-07 01:22:04 -0800

    Questions are now seperated into async function which allows for looping until user defines team build completion

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
 "use strict";
 
-const prompt = require("inquirer");
-const questions = require("./questions");
+const inquirer = require("inquirer");
+const questions = require("./allQuestions/questions");
+const internQuestions = require("./allQuestions/intern-questions.json");
+const engineerQuestions = require("./allQuestions/engineer-questions");
+const addEmployeeQuestions = require("./allQuestions/add-employee");
+const theTeam = [];
 
-function promptUser() {
-  return prompt(prompts);
+async function initialBuild() {
+  try {
+    const groundTeam = await inquirer.prompt(questions);
+    theTeam.push(groundTeam);
+    console.log(groundTeam);
+  } catch (err) {
+    console.log(err);
+  }
+  addEmployee();
 }
+async function addEmployee(groundTeam) {
+  try {
+    const buildEmployee = await inquirer.prompt(addEmployeeQuestions);
+    console.log(buildEmployee);
+    if (buildEmployee.newemployee === "Engineer") {
+      addEngineer();
+    } else if (buildEmployee.newemployee === "Intern") {
+      addIntern();
+    } else {
+      console.log(theTeam);
+      return "Team complete";
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+async function addIntern() {
+  try {
+    const newIntern = await inquirer.prompt(internQuestions);
+    console.log(newIntern);
+    theTeam.push(newIntern);
+  } catch (err) {
+    console.log(err);
+  }
+  addEmployee();
+}
+async function addEngineer() {
+  try {
+    const newEngineer = await inquirer.prompt(engineerQuestions);
+    console.log(newEngineer);
+    theTeam.push(newIntern);
+  } catch (err) {
+    console.log(err);
+  }
+  addEmployee();
+}
+
+initialBuild();
